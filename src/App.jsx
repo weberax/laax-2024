ca496e6fc024c5d9e428a2148097f6cd68f9a857
Axel Weber <weberal@studi.informatik.uni-stuttgart.de>
2023-11-09 00:15:31 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
 
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <Router basename={process.env.PUBLIC_PATH}>
+      <Router basename={"/laax-2024"}>
         <div className="App">
           <div className="container-fluid">
             <div className="row flex-nowrap">
